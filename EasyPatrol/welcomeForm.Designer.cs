6e0abc584eba245877d1f059f65ff9581f589f05
SticksDev <tsommers150@gmail.com>
2022-05-27 09:40:18 -0400

    set welcomeForms version to the correct one (oops)

--- EasyPatrol/welcomeForm.Designer.cs
+++ EasyPatrol/welcomeForm.Designer.cs
@@ -56,9 +56,9 @@ namespace EasyPatrol
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(-3, 438);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "v0.0.1 beta";
+            this.label2.Text = "v0.0.1.1";
             // 
             // label3
             // 
